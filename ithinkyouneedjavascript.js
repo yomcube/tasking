fbf2ae878bf2e9baf7e8b7fb1c0f1ce17515b336
TomCube <121310847+yomcube@users.noreply.github.com>
2023-01-16 00:27:33 -0600

    hide <hr>s when needed

--- ithinkyouneedjavascript.js
+++ ithinkyouneedjavascript.js
@@ -5,7 +5,7 @@ var todoUl         = document.getElementById("todo");
 var todoTasks      = todoUl.getElementsByTagName("li");
 
 function addNewTask(type, task) {
-	if(task == "" || task == " ") {
+	if(task == null || task == "" || task == " ") {
 		return;
 	}
 	var newelem = document.createElement("li");
@@ -19,6 +19,13 @@ function addNewTask(type, task) {
 		default:
 			newelem.id = `regular${regularTasks.length}`;
 			regularUl.appendChild(newelem);
+		
+		if (regularTasks.length >= 0) {
+			document.getElementById("reghr2").hidden == false;
+		}
+		if (todoTasks.length >= 0) {
+			document.getElementById("todohr2").hidden == false;
+		}
 	}
 }
 function removeTask(elem) {
