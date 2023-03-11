e57fee475ce855420ed4d9326677f24185c7b4c0
TomCube <121310847+yomcube@users.noreply.github.com>
2023-03-11 16:01:37 -0600

    changed stuff

--- ithinkyouneedjavascript.js
+++ ithinkyouneedjavascript.js
@@ -1,28 +1,22 @@
-var routineUl = document.getElementById('routineul');
-var otherUl = document.getElementById('otherul');
-
 function addNewTask(type, task) {
 	if(task == null || task == "" || task == " ") {
 		return;
 	}
 	var newelem = document.createElement("li");
-	newelem.innerHTML = task + ' <button class="removebutton" onclick="removeTask(this.parentNode)">&#10007;</button>';
+	newelem.innerHTML = task + ' <button class="removebutton" onclick="this.parentNode.remove();">&#10007;</button>';
 	if (type == 'o') {
-		otherUl.appendChild(newelem);
+		document.getElementById("otherul").appendChild(newelem);
 	} else if (type == 'r') {
-		routineUl.appendChild(newelem);
+		document.getElementById("routineul").appendChild(newelem);
 	} else {
 		console.log('Error E0001')
 	}
-}
-function removeTask(elem) {
-	elem.remove();
 }
 function saveTasks() {
 	localStorage.setItem("routine", routineUl.getElementsByTagName("li"));
 	localStorage.setItem("other", otherUl.getElementsByTagName("li"));
 }
 function loadTasks() {
-	routineUl.innerHTML = localStorage.getItem("routine");
-	otherUl.innerHTML = localStorage.getItem("other");
+	document.getElementById("routineul").innerHTML = localStorage.getItem("routine");
+	document.getElementById("otherul").innerHTML = localStorage.getItem("other");
 }
